19d31ae57ccd7669cc387287ef67c0fa7f19ccc3
vladis43 <vladfidem@gmail.com>
2018-11-29 11:47:37 +0200

    refactoring models

--- server/src/models/Todo.js
+++ server/src/models/Todo.js
@@ -3,19 +3,19 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const todo = new Schema({
-        title: {
-            type: Schema.Types.String,
-            required: true
-        },
-        completed: {
-            type: Schema.Types.Boolean,
-            default: false
-        },
-        userId: {
-            type: Schema.Types.ObjectId,
-            required: true
-        }
-    })
+    title: {
+        type: Schema.Types.String,
+        required: true
+    },
+    completed: {
+        type: Schema.Types.Boolean,
+        default: false
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    }
+})
 
 const Todo = mongoose.model('Todo', todo)
 
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -3,44 +3,44 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const user = new Schema({
-        username: {
-            type: Schema.Types.String,
-            required: true,
-            unique: true
-        },
-        email: {
-            type: Schema.Types.String,
-            required: true,
-            unique: true
-        },
-        password: {
-            type: Schema.Types.String,
-            required: true
-        },
-        age: {
-            type: Schema.Types.Number,
-            required: true,
-        },
-        sex: {
-            type: Schema.Types.String,
-            required: true,
-        },
-        country: {
-            type: Schema.Types.String,
-            required: true,
-        },
-        city: {
-            type: Schema.Types.String,
-            required: true,
-        },
-        verificationCode: {
-            type: Schema.Types.String
-        },
-        active: {
-            type: Schema.Types.Boolean,
-            default: false
-        }
-    })
+    username: {
+        type: Schema.Types.String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: Schema.Types.String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: Schema.Types.String,
+        required: true
+    },
+    age: {
+        type: Schema.Types.Number,
+        required: true,
+    },
+    sex: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    country: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    city: {
+        type: Schema.Types.String,
+        required: true,
+    },
+    verificationCode: {
+        type: Schema.Types.String
+    },
+    active: {
+        type: Schema.Types.Boolean,
+        default: false
+    }
+})
 
 const User = mongoose.model('User', user)
 
